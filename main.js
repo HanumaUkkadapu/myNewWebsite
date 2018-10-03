4e6a03ac972a0880843dd422a2f3f541098e96c3
Tulasi Veera Hanuma Ukkadapu <30723099+HanumaUkkadapu@users.noreply.github.com>
2018-10-03 20:25:21 +0530

    update 03_10_2018
    added some functionaloity to  the author element

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 
+/* here we used display instead of visibility because it makes as if the object is removed when the display is set to none. inncase of visibility, making it hidden still shows the presense of the element i.e. doesn't effct layout. */
+
+var x = document.getElementById("author-content") ;
+x.style.display = "none" ;
+
+function authorContent(){
+    if(x.style.display == "none"){
+        x.style.display = "block" ;
+    }
+}
+
+function closeAuthor(){
+    if(x.style.display == "block"){
+        x.style.display = "none" ;
+    }
+}
